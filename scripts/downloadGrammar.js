4b1e417556a622c0d08ca0d47b7e2620d425248c
fwcd <fwcdmail@gmail.com>
2022-03-22 02:24:03 +0100

    Use consistent 4-space-indentation and add editorconfig

--- scripts/downloadGrammar.js
+++ scripts/downloadGrammar.js
@@ -16,17 +16,17 @@ const DOWNLOAD_GRAMMAR_PATH = path.join(DOWNLOADS_PATH, GRAMMAR_NAME);
 
 console.log("Downloading grammars...");
 fs.promises.mkdir(DOWNLOADS_PATH, { recursive: true })
-	.then(() => {
-		request.get(GRAMMAR_ZIP_URL)
-			.on("complete", () => {
-				console.log("Extracting grammars...");
-				extractZip(DOWNLOAD_ZIP_PATH, { dir: DOWNLOADS_PATH })
-					.then(() => fs.promises.mkdir(GRAMMARS_PATH, { recursive: true }))
-					.then(() => fs.promises.copyFile(DOWNLOAD_GRAMMAR_PATH, GRAMMAR_PATH))
-					.then(() => fs.promises.unlink(DOWNLOAD_ZIP_PATH))
-					.catch(e => console.log(e));
-			})
-			.on("error", e => console.log(e))
-			.pipe(fs.createWriteStream(DOWNLOAD_ZIP_PATH));
-		})
-		.catch(e => console.log(e));
+    .then(() => {
+        request.get(GRAMMAR_ZIP_URL)
+            .on("complete", () => {
+                console.log("Extracting grammars...");
+                extractZip(DOWNLOAD_ZIP_PATH, { dir: DOWNLOADS_PATH })
+                    .then(() => fs.promises.mkdir(GRAMMARS_PATH, { recursive: true }))
+                    .then(() => fs.promises.copyFile(DOWNLOAD_GRAMMAR_PATH, GRAMMAR_PATH))
+                    .then(() => fs.promises.unlink(DOWNLOAD_ZIP_PATH))
+                    .catch(e => console.log(e));
+            })
+            .on("error", e => console.log(e))
+            .pipe(fs.createWriteStream(DOWNLOAD_ZIP_PATH));
+        })
+        .catch(e => console.log(e));
