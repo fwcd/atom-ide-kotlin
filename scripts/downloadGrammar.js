d90edc430ede45bb609aab2c3c62e5a40a8dca16
fwcd <fwcdmail@gmail.com>
2022-03-22 01:43:42 +0100

    Update download script

--- scripts/downloadGrammar.js
+++ scripts/downloadGrammar.js
@@ -6,22 +6,27 @@ const path = require("path");
 const extractZip = require("extract-zip");
 const fs = require("fs");
 
-const GRAMMAR_URL = "https://github.com/fwcd/kotlin-language-server/releases/latest/download/grammars.zip";
+const GRAMMAR_ZIP_URL = "https://github.com/fwcd/kotlin-language-server/releases/latest/download/grammars.zip";
+const GRAMMAR_NAME = "Kotlin.tmLanguage.json";
 const GRAMMARS_PATH = path.join(__dirname, "..", "grammars");
-const DOWNLOAD_PATH = path.join(GRAMMARS_PATH, "grammarsDownload.zip");
+const GRAMMAR_PATH = path.join(GRAMMARS_PATH, GRAMMAR_NAME);
+const DOWNLOADS_PATH = path.join(__dirname, "..", "downloads");
+const DOWNLOAD_ZIP_PATH = path.join(DOWNLOADS_PATH, "grammars.zip");
+const DOWNLOAD_GRAMMAR_PATH = path.join(DOWNLOADS_PATH, GRAMMAR_NAME);
 
 console.log("Downloading grammars...");
-request.get(GRAMMAR_URL)
-	.on("complete", () => {
-		console.log("Extracting grammars...");
-		extractZip(DOWNLOAD_PATH, { dir: GRAMMARS_PATH })
-			.then(() => {
-				console.log("Cleaning up downloaded zip...");
-				fs.unlink(DOWNLOAD_PATH, err => {
-					if (err) console.log(err);
-				});
+fs.promises.mkdir(DOWNLOADS_PATH, { recursive: true })
+	.then(() => {
+		request.get(GRAMMAR_ZIP_URL)
+			.on("complete", () => {
+				console.log("Extracting grammars...");
+				extractZip(DOWNLOAD_ZIP_PATH, { dir: DOWNLOADS_PATH })
+					.then(() => fs.promises.mkdir(GRAMMARS_PATH, { recursive: true }))
+					.then(() => fs.promises.copyFile(DOWNLOAD_GRAMMAR_PATH, GRAMMAR_PATH))
+					.then(() => fs.promises.unlink(DOWNLOAD_ZIP_PATH))
+					.catch(e => console.log(e));
 			})
-			.catch(e => console.log(e));
-	})
-	.on("error", err => console.log(err))
-	.pipe(fs.createWriteStream(DOWNLOAD_PATH));
+			.on("error", e => console.log(e))
+			.pipe(fs.createWriteStream(DOWNLOAD_ZIP_PATH));
+		})
+		.catch(e => console.log(e));
